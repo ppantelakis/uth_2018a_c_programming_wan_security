29ef175e9a5b729b0c60f89a5666e1cdb938e7f8
Panagiotis Pantelakis <panagiotis.pantelakis@gmail.com>
2018-03-27 10:01:23 +0300

    TCP Size

--- was.c
+++ was.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #define PORT1 43657
 #define PORT2 55981
+//IP Header Length 
+//https://en.wikipedia.org/wiki/IPv4
+//20 bytes = 160 bits
+//
+//TCP Header Length
+//https://en.wikipedia.org/wiki/Transmission_Control_Protocol
 main()
 {
+	printf("Port sequence first:%d, second:%d\n",PORT1,PORT2);
 	return 0;
 }
