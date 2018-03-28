322fd42f79083cf4cf64b84658158d933532bd34
George Mitrou <georgebitzi@gmail.com>
2018-03-29 01:54:42 +0300

    Added log initialisation and some logs in main

--- was.c
+++ was.c
@@ -12,6 +12,11 @@ main()
     char *currdt = get_curr_time();
     printf("Port sequence first:%d, second:%d\n",PORT1,PORT2);
     printf("Daemon has started at:%s\n",get_curr_time());
+    //Set the log level
+    //http://www.gnu.org/software/libc/manual/html_node/setlogmask.html
+    setlogmask( LOG_UPTO(LOG_INFO) );
+    openlog( "WAS", LOG_CONS, LOG_USER );
+
     return 0;
 }
 
