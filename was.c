d97dc4227c7be16584674a0f70d60b38b2b5cdc8
Panagiotis Pantelakis <panagiotis.pantelakis@gmail.com>
2018-03-28 14:43:44 +0300

    Added time started daemon

--- was.c
+++ was.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <time.h>
 #include "was_globals.h"
 #include "was_structs.h"
 #include "was_functions.h"
 
 main()
 {
-        printf("Port sequence first:%d, second:%d\n",PORT1,PORT2);
-        return 0;
+    char buff_time[100];
+    time_t now = time (0);
+    strftime (buff_time, 100, "%Y-%m-%d %H:%M:%S", localtime (&now));
+    printf("Port sequence first:%d, second:%d\n",PORT1,PORT2);
+    printf("Daemon has started at:%s\n",buff_time);
+    return 0;
 }
