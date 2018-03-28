b4825c87530cbf2193e69932bda412e15c3575a2
George Mitrou <georgebitzi@gmail.com>
2018-03-29 02:03:33 +0300

    Organized the function main

--- was.c
+++ was.c
@@ -9,12 +9,13 @@
 char* get_curr_time();
 main()
 {
-    char *currdt = get_curr_time();
     printf("Port sequence first:%d, second:%d\n",PORT1,PORT2);
     printf("Daemon has started at:%s\n",get_curr_time());
     //Set the log level
     //http://www.gnu.org/software/libc/manual/html_node/setlogmask.html
+    //https://linux.die.net/man/3/setlogmask
     setlogmask( LOG_UPTO(LOG_INFO) );
+    //http://www.gnu.org/software/libc/manual/html_node/openlog.html
     openlog( "WAS", LOG_CONS, LOG_USER );
 
     return 0;
